788077d7fffce0073f7815aa3ee3d1e4f16e3cd2
emmyrunge <erunge@student.touro.edu>
2023-05-11 15:11:51 -0400

    added image and degree labels

--- src/main/java/runge/weather/CurrentWeatherFrame.java
+++ src/main/java/runge/weather/CurrentWeatherFrame.java
@@ -1,14 +1,18 @@
 package runge.weather;
 
 import javax.inject.Inject;
+import javax.inject.Named;
 import javax.swing.*;
 import java.awt.*;
 
 public class CurrentWeatherFrame extends JFrame
 {
-
     @Inject
-    public CurrentWeatherFrame(CurrentWeatherView view, ForecastWeatherController controller) {
+    public CurrentWeatherFrame(CurrentWeatherView view,
+                               ForecastWeatherController controller,
+                               CurrentWeatherController currentWeatherController,
+                               @Named("imageLabel") JLabel imageLabel,
+                               @Named("degreeLabel") JLabel degreeLabel) {
 
 
         setSize(780, 500);
@@ -19,6 +23,11 @@ public class CurrentWeatherFrame extends JFrame
         mainPanel.setLayout(new BorderLayout());
 
         JPanel northPanel = new JPanel(new BorderLayout());
+        JPanel currentWeatherPanel = new JPanel();
+
+        currentWeatherPanel.add(imageLabel);
+        currentWeatherPanel.add(degreeLabel);
+        northPanel.add(currentWeatherPanel, BorderLayout.SOUTH);
 
         //BUG: button only pops up after it is clicked...
         JButton updateButton = new JButton("Update");
@@ -31,8 +40,15 @@ public class CurrentWeatherFrame extends JFrame
 
         setContentPane(mainPanel);
 
-        updateButton.addActionListener(e -> controller.updateLocation(enterCityField.getText()));
+        updateButton.addActionListener(e ->
+        {
+            controller.updateLocation(enterCityField.getText());
+            currentWeatherController.updateWeather(enterCityField.getText());
+        });
         controller.updateLocation(enterCityField.getText());
+        currentWeatherController.updateWeather(enterCityField.getText());
+
+
 
     }
 
